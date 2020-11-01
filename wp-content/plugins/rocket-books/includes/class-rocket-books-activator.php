14bf6e569e3b8ca27e972593d77aeeff7237722f
Jean-PierrePapin <skouo82@gmail.com>
2020-11-01 11:21:48 +0000

    Installed the WP boilerplate

---
+++ wp-content/plugins/rocket-books/includes/class-rocket-books-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://livres.io
+ * @since      1.0.0
+ *
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/includes
+ * @author     James Lecodeur <Ma-h3sW3YEWRip=i@5E#>
+ */
+class Rocket_Books_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
--- wp-content/plugins/very-simple-event-list/js/vsel-datepicker.js
+++
@@ -1,7 +0,0 @@
-/* Very Simple Event List datepicker */
-
-jQuery(document).ready(function ($) {
-	$( "#vsel-start-date, #vsel-end-date" ).datepicker({
-		dateFormat: objectL10n.dateFormat
-	});
-});
