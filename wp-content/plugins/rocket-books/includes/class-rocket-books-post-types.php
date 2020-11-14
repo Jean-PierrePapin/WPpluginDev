7f5f86853db8626f0151cb08687112d27dde77a5
Jean-PierrePapin <skouo82@gmail.com>
2020-11-14 10:08:49 +0000

    Completed nonce fields and nonce verification

--- wp-content/plugins/rocket-books/includes/class-rocket-books-post-types.php
+++ wp-content/plugins/rocket-books/includes/class-rocket-books-post-types.php
@@ -253,6 +253,8 @@ class Rocket_Books_Post_Types {
 
 		//echo 'Here, we shall display fields';
 
+		wp_nonce_field( 'rbr_meta_box_nonce_action', 'rbr_meta_box_nonce' );
+
 		?>
 		<label for="rbr-book-pages">
 			<?php _e( 'Number of Pages', 'rocket-books' ); ?>
@@ -273,6 +275,16 @@ class Rocket_Books_Post_Types {
 	 * Saving Custom fields for CPT: book
 	 */
 	public function metabox_save_book( $post_id, $post, $update ) {
+
+		// Verify Nonce
+		if ( ! isset( $_POST['rbr_meta_box_nonce'] ) 
+			||
+			! wp_verify_nonce( $_POST['rbr_meta_box_nonce'], 'rbr_meta_box_nonce_action' 
+			)
+		) {
+			print __( 'Sorry your nonce did not verify', 'rocket-books' );
+			exit;
+		}
 
 		//var_export($_POST);
 
