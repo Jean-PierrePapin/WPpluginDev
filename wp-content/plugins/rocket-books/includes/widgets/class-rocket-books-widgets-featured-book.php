f0bb1ea8cb792bcd3464bdec35e6e05a875ac0e6
Jean-PierrePapin <skouo82@gmail.com>
2021-01-11 19:42:22 +0000

    Helper Class Part 2 adding fields

--- wp-content/plugins/rocket-books/includes/widgets/class-rocket-books-widgets-featured-book.php
+++ wp-content/plugins/rocket-books/includes/widgets/class-rocket-books-widgets-featured-book.php
@@ -21,13 +21,63 @@ if ( ! class_exists( 'Rocket_Books_Widget_Featured_Book' ) ) {
                 'id'            => 'rbr_featured_book',
                 'name'          => __( 'Featured Book', 'rocket-books' ),
                 'desc'          => __( 'Display your Featured Book', 'rocket-books' )
-            ];
+			];
+			
+			$this->set_fields( $this->get_fields_args() );
             
 			parent::__construct( $config_array );
 		}
 
+		/**
+		 * fields arguments array
+		 */
+		public function get_fields_args() {
+
+			$fields_args = [
+				[
+					'id'	=> 'title',
+					'label'	=> __( 'Title', 'rocket-books' )
+				],
+				[
+					'id'	=> 'text_color',
+					'type'	=> 'color',
+					'label' => __( 'Text Color', 'rocket-books' )
+				],
+				[
+					'id'	=> 'bgcolor',
+					'type'	=> 'color',
+					'label' => __( 'Background Color', 'rocket-books' )
+				],
+			];
+
+			return $fields_args;
+		}
+
+		/**
+		 * Display widget after the title
+		 */
+		public function widget_display( $args, $instance ) {
+/* 
+			echo "<pre>";
+			var_export($instance);
+			echo "</pre>";
+ */
+			// text color
+			$text_color = isset( $instance['text_color'] ) ? $instance['text_color'] : '';
+			// bg color
+			$bgcolor = isset( $instance['bgcolor'] ) ? $instance['bgcolor'] : '';
+			// post id to be shown
 
 
+
+			echo do_shortcode( "[book_list 
+									column=1 
+									limit=1
+									color={$text_color}
+									bgcolor={$bgcolor}
+									]" );
+		}
+		
 
 
 	}
